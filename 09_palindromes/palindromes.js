7311abd35ce622b2d21884eceb970b7aefaeb18e
MartinGarzaFCFM <martin.villanuevagrz@uanl.edu.mx>
2022-06-21 13:56:20 -0500

    Solved palidrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (txt) {
+    let clnTxt = txt.replace(/[^\w]/g, '');
+    clnTxt = clnTxt.toLowerCase();
+    let newTxt = "";
+    for (let i = clnTxt.length-1; i >= 0; i--) {
+        newTxt += clnTxt[i];
+    }
+    return newTxt === clnTxt;
 };
 
 // Do not edit below this line
