d53ce292ae075249c81f5f206b0c3f09386218c1
MartinGarzaFCFM <martin.villanuevagrz@uanl.edu.mx>
2022-06-24 14:33:09 -0500

    Solved 10, 11, 12 JS exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n > 0) {
+        let a = 0;
+        let b  = 1;
+        let sum = a + b;
+        for (let i = 1; i < n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+    else {
+        return "OOPS";
+    }
 };
+
+/*
+    sum = 0, 1, 2, 3, 5, 8
+    a = 0, 1, 1, 2, 3, 5
+    b = 1, 1, 2, 3, 5, 8
+
+    0 + 1 = 1
+        1 + 1 = 2
+            1 + 2 = 3
+                2 + 3 = 5
+                    3 + 5 = 8
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
